9cc2df9b71c2c3d30cdb0f8ee9789252841f0d98
victorv <victorvega.v@gmail.com>
2024-09-19 13:52:03 -0300

    Added auth subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,41 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 
-import MarketingApp from "./components/MarketingApp";
+import Progress from "./components/Progress";
 import Header from "./components/Header";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
 export default () => {
+  const [isSignedin, setSignedIn] = useState(false);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header
+            onSignOut={() => setSignedIn(false)}
+            isSignedIn={isSignedin}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setSignedIn(true)} />
+              </Route>
+              <Route path="/">
+                <MarketingLazy onSignIn={() => setSignedIn(true)} />
+              </Route>
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
